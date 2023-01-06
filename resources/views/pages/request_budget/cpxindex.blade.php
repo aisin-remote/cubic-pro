c3898fe69881812fb34642099514f5fac5ef65fb
AIIA\rafie <rafie@aiia.co.id>
2023-01-06 13:47:08 +0700

    DEV 1.1.0 : Remove Gate Download on Budget Account (RB)

--- resources/views/pages/request_budget/cpxindex.blade.php
+++ resources/views/pages/request_budget/cpxindex.blade.php
@@ -147,6 +147,7 @@
     <?php
     use App\Department;
     $dept_id = auth()->user()->department_id;
+    $email =  auth()->user()->email;
     $deptuser = Department::where('id', '=', $dept_id)->first()->department_code;
     ?>
     <script>
@@ -341,12 +342,13 @@
 
             $('#btn-download').click(function() {
                 var depuser = "<?php echo $deptuser; ?>";
+                var email = "<?php echo $email; ?>";
 
                 var dept = $('#dept').val()
 
                 if (dept == "") {
                     return false
-                } else if (dept != depuser) {
+                } else if (dept != depuser && email != "budget@aiia.co.id") {
                     Swal.fire({
                         icon: 'error',
                         title: 'Oops...',
